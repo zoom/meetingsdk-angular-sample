47a9c71762393188d64e082d595f9458c21a26a7
tommygaessler <tommy.gaessler@gmail.com>
2023-01-19 12:28:25 -0700

    Updating to 2.9.7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { DOCUMENT } from '@angular/common';
 
 import { ZoomMtg } from '@zoomus/websdk';
 
-ZoomMtg.setZoomJSLib('https://source.zoom.us/2.9.5/lib', '/av');
+ZoomMtg.setZoomJSLib('https://source.zoom.us/2.9.7/lib', '/av');
 
 ZoomMtg.preLoadWasm();
 ZoomMtg.prepareWebSDK();
