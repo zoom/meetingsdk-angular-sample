9da6179f8b661f351c98c4b4c32bc3ad71ca89dc
tommygaessler <tommy.gaessler@gmail.com>
2023-06-02 16:31:44 -0600

    updated to 2.13.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { DOCUMENT } from '@angular/common';
 
 import { ZoomMtg } from '@zoomus/websdk';
 
-ZoomMtg.setZoomJSLib('https://source.zoom.us/2.12.2/lib', '/av');
+ZoomMtg.setZoomJSLib('https://source.zoom.us/2.13.0/lib', '/av');
 
 ZoomMtg.preLoadWasm();
 ZoomMtg.prepareWebSDK();
